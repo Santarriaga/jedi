51d65dc933ca67e269eb3ad76add1164fc21cef3
Santarriaga <ed.santarriaga@gmail.com>
2020-07-27 14:43:20 -0700

    commit test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const firebase = require("firebase");
+require("firebase/firestore");
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAgh7_qX8iqXqF5O6ma96klsGucwpsv7pE",
+    authDomain: "jedi-e1061.firebaseapp.com",
+    databaseURL: "https://jedi-e1061.firebaseio.com",
+    projectId: "jedi-e1061",
+    storageBucket: "jedi-e1061.appspot.com",
+    messagingSenderId: "216801536791",
+    appId: "1:216801536791:web:52a0fd17ff2fd5c77c2552",
+    measurementId: "G-K121L84V73"
+});
+
+ReactDOM.render(<div>Hello world </div>,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
