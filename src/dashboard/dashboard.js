ad83b03d9f01f9ea7ccec85a1f22edd305b196e8
Santarriaga <ed.santarriaga@gmail.com>
2020-07-28 14:04:13 -0700

    can send messages now

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ChatListComponent from '../chatList/chatList';
 import ChatViewComponent from '../chatView/chatView';
+import ChatTextBoxComponent from '../chatTextBox/chatTextBox';
 import {Button, withStyles} from '@material-ui/core';
 import styles from './styles'
 
@@ -29,9 +30,7 @@ class DashboardComponent extends React.Component{
                 selectChatFn={this.selectChat}
                 chats={this.state.chats}
                 userEmail={this.state.email}
-                selectedChatIndex={this.state.selectedChat}>
-                </ChatListComponent>
-                <Button className={classes.signOutBtn} onClick={this.signOut}> Sign Out</Button>
+                selectedChatIndex={this.state.selectedChat}></ChatListComponent>
                 {
                     this.state.newChatFormVisible ?
                     null:
@@ -40,6 +39,13 @@ class DashboardComponent extends React.Component{
                         chat={this.state.chats[this.state.selectedChat]}
                         ></ChatViewComponent>
                 }
+                {
+                    this.state.selectedChat !== null && !this.state.newChatFormVisible ?
+                    <ChatTextBoxComponent messageReadFn={this.messageRead}submitMessageFn={this.submitMessage} ></ChatTextBoxComponent> :
+                    null
+                }
+                <Button className={classes.signOutBtn} onClick={this.signOut}> Sign Out</Button>
+
             </div>
         );
     }
@@ -48,11 +54,50 @@ class DashboardComponent extends React.Component{
     signOut = () => firebase.auth().signOut();
 
     //props for chatlist
-    selectChat = (chatIndex) => {
-        this.setState({selectedChat: chatIndex});
+    selectChat = async (chatIndex) => {
+        await this.setState({selectedChat: chatIndex});
+        this.messageRead();
     }
     //props fo chatlist
     newChatBtnClicked = () => this.setState({newChatFormVisible: true, selectedChat: null})
+
+
+    //
+    submitMessage =(msg) => {
+        const docKey = this.buildDocKey(this.state.chats[this.state.selectedChat].users.filter(_usr => _usr !== this.state.email)[0]);
+        firebase
+            .firestore()
+            .collection('chats')
+            .doc(docKey)
+            .update({
+                messages: firebase.firestore.FieldValue.arrayUnion({
+                    sender: this.state.email,
+                    message: msg,
+                    timestamp: Date.now()
+                }),
+                receiverHasRead: false
+            });
+    }
+
+
+    messageRead = () =>{
+        const docKey = this.buildDocKey(this.state.chats[this.state.selectedChat].users.filter(_usr => _usr !== this.state.email)[0]);
+        if(this.clickedChatWhereNotSender(this.state.selectedChat)){
+            firebase
+                .firestore()
+                .collection('chats')
+                .doc(docKey)
+                .update({receiverHasRead: true })
+        }else{
+            console.log('error');
+        }
+    }
+
+    clickedChatWhereNotSender = (chatIndex) => this.state.chats[chatIndex].messages[this.state.chats[chatIndex].messages.length -1].sender !== this.state.email;
+
+    //function for
+    buildDocKey = (friend) => [this.state.email, friend].sort().join(':');
+
 
     // firebase
     componentDidMount = () => {
